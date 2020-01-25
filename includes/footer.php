dc1c66a145244937cf2807f4d6b21efb0739dc0c
BlackWhite <39222378+BlackWhite2000@users.noreply.github.com>
2020-01-25 21:02:58 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 <div class="container">
   <div class="footer-height-50px"></div>
                 <section class="footer-float-left footer-align-center">
-                	<?php if (($this->options->build_time) == ''): ?><p>Copyright © <?php echo date('Y '); ?><?php else: ?><p>Copyright © <?php $this->options->build_time(); ?>-<?php echo date('Y '); ?><?php endif; ?> <a href="#" target=_self>BlackWhite</a></p>
+                	<?php if (($this->options->build_time) == ''): ?><p>Copyright © <?php echo date('Y '); ?><?php else: ?><p>Copyright © <?php $this->options->build_time(); ?>-<?php echo date('Y '); ?><?php endif; ?> <a href="#" target=_self><?php $this->author() ?></a></p>
                <?php $this->options->footerleft(); ?>
                 </section>
                 <section class="footer-float-right footer-align-center">
