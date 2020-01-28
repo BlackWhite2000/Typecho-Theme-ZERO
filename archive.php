82df07d0099652c48f86b751df7ec87999c316a8
BlackWhite <39222378+BlackWhite2000@users.noreply.github.com>
2020-01-28 12:30:35 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -45,10 +45,7 @@ $this->need('includes/header.php');
 
 <div class="clearfloat"></div>
 </div>  
-<div class="pages">
-                <?php $this->pageLink('上一页'); ?> &nbsp;
-                <?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?>&nbsp;/&nbsp;<?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?>&nbsp;
-                <?php $this->pageLink('下一页','next'); ?></div>
-            </div>
+   <div class="pages"><?php $this->pageLink('上一页'); ?>&nbsp;&nbsp;<?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?>&nbsp;/&nbsp;<?php echo ceil($this->getTotal() / $this->parameter->pageSize); ?>&nbsp;&nbsp;<?php $this->pageLink('下一页','next'); ?></div> </div>
+            
 	</main>
 <?php $this->need('includes/footer.php'); ?>
